1dddaefa7e2df420cb95829bf3b2ac500588f7fe
VT10A <152657392+VT10A@users.noreply.github.com>
2024-07-17 14:22:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ import plotly.express as px
 # Define the text for chat completions
 chat_completion_text = (
     "Exclude the 'Other' topic from the following dataset and then rank the percentages for each topic in descending order and store the top four in your memory. Summarise these top 4 topics in two sentences, "
-    "in the format X, Y, and Z are the top 4 cited themes. Make sure your commentry focuses on "
-    "the items that have the highest percentages only (rank the top 4). Don't use decimals. "
+    "in the format X, Y, B and Z are the top 4 cited themes."
+    "tDon't use decimals."
     "Here's the data {topic_percentages}."
 )
 
